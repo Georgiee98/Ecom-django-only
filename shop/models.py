b83e5edef6b811b1f3a20a50938e45a1aec05bc9
“Gjorgi <georgiedelev@gmail.com>
2023-05-03 13:41:01 +0200

    Models for the colleciton of the form

--- shop/models.py
+++ shop/models.py
@@ -12,3 +12,14 @@ class Product(models.Model):
 
     def __str__(self):
         return f"ID: {self.id} | {self.title} |"
+
+
+class Order(models.Model):
+    items = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    adress = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    zip_code = models.CharField(max_length=255)
+    ordered = models.BooleanField(default=False)
